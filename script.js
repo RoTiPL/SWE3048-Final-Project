8422d76e83f1b10c2d403cf9497cfbc431d4bfbb
CheShireCat0430 <vickey043011@gmail.com>
2021-12-12 16:08:15 +0900

    fix equipment

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(function () {
 });
 
 
-
+var sel_file;
 
 // Setup the calendar with the current date
 $(document).ready(function(){
@@ -30,11 +30,76 @@ $(document).ready(function(){
     var events = check_events(today, date.getMonth()+1, date.getFullYear());
     show_events(events, months[date.getMonth()], today);
 
-    $("#addEquipBtn").click(function() {location.reload()});
     $("#saveUser").click(function() {location.reload()});
     $("#saveRegister").click(function() {location.reload()});
     $("#saveEquipState").click(function() {location.reload()});
+
+    var forms = document.querySelectorAll('.needs-validation')
+
+    $("#customFile").on("change", handleImgsFilesSelect);
+    $("#addEquipBtn").click(function() {
+        // downloadImg();
+        // alert("hello");
+        location.reload()
+    });
+
+//   // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms)
+//         .forEach(function (form) {
+//         form.addEventListener('submit', function (event) {
+//             if (!form.checkValidity()) {
+//             event.preventDefault()
+//             event.stopPropagation()
+//             }
+
+//             form.classList.add('was-validated')
+//         }, false)
+//     })
 });
+
+// Show imagefile at add equipment image
+function handleImgsFilesSelect(e) {
+    var files = e.target.files;
+    var filesArr = Array.prototype.slice.call(files);
+
+    filesArr.forEach(function(f) {
+        if(!f.type.match("image.*")) {
+            alert("It is not image file");
+            return;
+        }
+
+        sel_files = f;
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $("#img").attr("src", e.target.result);
+        }
+        reader.readAsDataURL(f);
+    });
+}
+
+// For download image file at add equipment image
+function downloadImg(){
+    const imageInput = $("#customFile")[0];
+    console.log("imageInput: ", imageInput.files)
+
+    const formData = new FormData();
+    formData.append("image", imageInput.files[0]);
+
+    $.ajax({ 
+        type: 'POST', 
+        url: 'assets/equip', 
+        processData: false, // 필수 
+        contentType: false, // 필수 
+        data: formData, 
+        success: function(rtn){
+            console.log("message: ", rtn)
+          },
+          err: function(err){
+            console.log("err:", err)
+          }
+    });
+}
 
 // Initialize the calendar by appending the HTML dates
 function init_calendar(date) {
